5fb4e242ec88de368341cb3f7c1df2219371efd1
Julian Reif <42722121+reifjulian@users.noreply.github.com>
2020-06-25 09:40:17 -0500

    Update _install_R_packages.R

--- analysis/scripts/programs/_install_R_packages.R
+++ analysis/scripts/programs/_install_R_packages.R
@@ -7,8 +7,7 @@
 packages <- c("tidyverse","estimatr")
 
 # Set repository from which R packages will be downloaded
-# Note: the RStudio package manager enables users to install all packages from binary, including for Linux
-options(repos = "https://packagemanager.rstudio.com/all/latest")
+options(repos = "https://cran.rstudio.com")
 
 # Install new packages to first element of .libPaths() or, if path is not writeable, to default user library path
 lib <- .libPaths()[1]
@@ -17,6 +16,22 @@ if (!dir.create(lib, showWarnings = FALSE)[1]) {
   dir.create(lib, showWarnings = FALSE, recursive = TRUE)
 }
 
+# Function to determine whether user is running osx, linux, or something else
+get_os <- function(){
+  sysinf <- Sys.info()
+  if (!is.null(sysinf)){
+    os <- sysinf['sysname']
+    if (os == 'Darwin')
+      os <- "osx"
+  } else { ## mystery machine
+    os <- .Platform$OS.type
+    if (grepl("^darwin", R.version$os))
+      os <- "osx"
+    if (grepl("linux-gnu", R.version$os))
+      os <- "linux"
+  }
+  tolower(os)
+}
 
 ########
 # Sample code to install packages locally into /scripts/libraries/R/os instead of usual library path
@@ -24,33 +39,21 @@ if (!dir.create(lib, showWarnings = FALSE)[1]) {
 # Note: R packages such as tidyverse are large (>100 megabytes)
 ########
 
-# Function to determine whether user is running os is osx, linux, or something else
-#get_os <- function(){
-#  sysinf <- Sys.info()
-#  if (!is.null(sysinf)){
-#    os <- sysinf['sysname']
-#    if (os == 'Darwin')
-#      os <- "osx"
-#  } else { ## mystery machine
-#    os <- .Platform$OS.type
-#    if (grepl("^darwin", R.version$os))
-#      os <- "osx"
-#    if (grepl("linux-gnu", R.version$os))
-#      os <- "linux"
-#  }
-#  tolower(os)
-#}
-# os <- get_os()
-
 #proj_dir <- Sys.getenv(c("MyProject"))
 #dir.create(file.path(proj_dir, "scripts/libraries"))
 #dir.create(file.path(proj_dir, "scripts/libraries/R"))
-#dir.create(file.path(proj_dir, paste0("scripts/libraries/R/",os)))
-#lib <-     file.path(proj_dir, paste0("scripts/libraries/R/",os))
+#dir.create(file.path(proj_dir, paste0("scripts/libraries/R/",get_os())))
+#lib <-     file.path(proj_dir, paste0("scripts/libraries/R/",get_os()))
 
 ########
 # End of sample code
 ########
 
-# Install packages from binary
-lapply(packages, install.packages, lib = lib, dependencies=c("Depends", "Imports", "LinkingTo"))
+
+# Install packages from binary, unless system is Unix (where source is only option)
+install_type <- "binary"
+if (.Platform$OS.type=="unix" & get_os()!="osx") {
+  	install_type <- "source"
+}
+
+lapply(packages, install.packages, lib = lib, type = install_type, dependencies=c("Depends", "Imports", "LinkingTo"))
